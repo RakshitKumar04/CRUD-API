564ec2be5a6087f57b204dfd2bd3fedfc9beca47
RakshitKumar04 <kumarrakshit0402@gmail.com>
2024-08-12 23:39:19 +0530

    Initailized Update Movie Function

--- main.go
+++ main.go
@@ -54,6 +54,24 @@ func createMovie(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movie)
 }
 
+// Update Movie
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get Params
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
 // Delete Movie
 func deleteMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
