54cfc8c7b4d84a48f6c635db2f94eb662bd9ad9a
RakshitKumar04 <kumarrakshit0402@gmail.com>
2024-08-12 23:14:46 +0530

    Initailized Libraries and Struct

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
